db58657f43cb8a9a238f087a76a68da78213beb5
spectrox0 <alejanvelazco2008@hotmail.com>
2020-07-12 00:59:20 -0400

    test

--- jest.config.js
+++ jest.config.js
@@ -1,23 +1,21 @@
 module.exports = {
-  testEnvironment: 'node',
-  roots: ['<rootDir>/components'],
-  preset: 'ts-jest',
-  setupFilesAfterEnv: ['<rootDir>/tests/setupTests.ts'],
+  moduleFileExtensions: ['ts', 'tsx', 'js'],
   transform: {
     '^.+\\.tsx?$': 'ts-jest',
   },
-  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-  testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
-  snapshotSerializers: ['enzyme-to-json/serializer'],
-
-  // https://github.com/zeit/next.js/issues/8663#issue-490553899
+  testMatch: ['**/*.(test|spec).(ts|tsx)'],
   globals: {
-    // we must specify a custom tsconfig for tests because we need the typescript transform
-    // to transform jsx into js rather than leaving it jsx such as the next build requires. you
-    // can see this setting in tsconfig.jest.json -> "jsx": "react"
     'ts-jest': {
-      tsConfig: '<rootDir>/tsconfig.jest.json',
+      babelConfig: true,
+      tsConfig: 'jest.tsconfig.json',
+      diagnostics: false,
     },
+  },
+  coveragePathIgnorePatterns: ['/node_modules/', 'enzyme.js'],
+  setupFilesAfterEnv: ['<rootDir>/enzyme.js'],
+  coverageReporters: ['json', 'lcov', 'text', 'text-summary'],
+  moduleNameMapper: {
+    '\\.(css|less|sass|scss)$': '<rootDir>/__mocks__/styleMock.js',
+    '\\.(gif|ttf|eot|svg)$': '<rootDir>/__mocks__/fileMock.js',
   },
 };
